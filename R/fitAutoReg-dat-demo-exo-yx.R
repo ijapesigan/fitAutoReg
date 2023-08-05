40b3431149b77f72ff39cf5d9307ce4c1761926f
ijapesigan <ijapesigan@gmail.com>
2023-08-05 14:09:21 +0000

    Automated build.

--- R/fitAutoReg-dat-demo-exo-yx.R
+++ R/fitAutoReg-dat-demo-exo-yx.R
@@ -1,9 +1,11 @@
 #' Data from the Vector Autoregressive Model (Y) and Lagged Predictors
 #' and Exogenous Variables (X)
 #'
-#' @format A list with elements Y and X where Y is equal to the `dat_demo_exo`
+#' @format A list with elements Y and X where Y
+#'   is equal to the `dat_demo_exo`
 #'   data set minus p = 2 terminal rows
 #'   and `X` is a matrix of ones for the first column
-#'   and lagged values of `Y` and exogenous variables for the rest of the columns.
+#'   and lagged values of `Y` and exogenous variables
+#'   for the rest of the columns.
 #' @keywords fitAutoReg data
 "dat_demo_exo_yx"
